cf3d3a6eeb0c02340f0a4f729cf43b2728626d73
Garrett Cross <garrettcrosskc@gmail.com>
2025-04-17 23:07:31 -0500

    Corrected string reversal logic.

--- src/StringChallenge.java
+++ src/StringChallenge.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
 public class StringChallenge {
+    public static String reverseSentence(String[] sen) {
+        StringBuilder result = new StringBuilder();
+
+        for (String word : sen) {
+            StringBuilder reversedWord = new StringBuilder(word);
+
+            result.append(reversedWord.reverse().append(" "));
+
+        }
+
+        return result.toString();
+    }
 
     public static void main(String[] args) {
-        StringBuilder sentence;
+        String sentence;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a sentence: ");
-        sentence = new StringBuilder(scanner.nextLine());
+        sentence = scanner.nextLine();
 
-        System.out.println("The sentence you entered in reverse is: " + sentence.reverse());
+        String[] splitSentence = sentence.split(" ");
+        String reversedSentence = reverseSentence(splitSentence);
+
+        System.out.println("The sentence you entered in reverse is: " + reversedSentence);
 
     }
 
